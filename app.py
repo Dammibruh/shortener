cec125369a7bfad66313b60bcfe40b2883103b41
dammi <36568916+Dammibruh@users.noreply.github.com>
2020-07-07 22:57:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import time
 import re
 import string
 import psycopg2
+import os
 from datetime import datetime
 import random
 
@@ -10,7 +11,8 @@ import random
 app = Flask(__name__)
 
 page = {"name" : "MIURL"}
-conn = psycopg2.connect("postgres://emacozuyzktjlc:8b473b403bf17b860820293e9e75e72935b43bd132267d78b71f129dae070ffd@ec2-52-72-221-20.compute-1.amazonaws.com:5432/d7jgm34tiabmtm")
+uri = os.environ["DATABASE_URL"]
+conn = psycopg2.connect(uri)
 cur = conn.cursor()
 cur.execute("CREATE TABLE IF NOT EXISTS shortlmao (url TEXT, shortened TEXT, requester TEXT, date TEXT)")
 
@@ -20,9 +22,7 @@ cur.execute("CREATE TABLE IF NOT EXISTS shortlmao (url TEXT, shortened TEXT, req
 def main():
 	return render_template("home.html", page=page)
 
-#hmm optional shit just copy main page design
 
-#shortener page design bruh just copy bootstrap
 @app.route("/shortener/", methods=["POST", "GET"])
 def shortener():
 	if request.method == "POST":
@@ -39,26 +39,6 @@ def shortener():
 			return render_template("index.html", page=page, urlsh="Invalid url provided"), 406
 	return render_template("index.html", page=page)
 
-#shortener api bruh advanced shit use poggersql after
-'''
-@app.route("/shortener/post/", methods=[ "POST", "GET"])
-def api():
-	if request.method == "POST":
-		if re.match("http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+",url):
-			requester_ip = request.remote_addr
-			name = "".join(random.choices(string.hexdigits, k=12))
-			date = str(datetime.utcnow())
-			url = request.form.get("url")
-			shortened = f"{request.url_root}shortener/{name}/"
-			cur.execute("INSERT INTO shortener (url, shortened, requester, date) VALUES(%s, %s, %s, %s)",(url, shortened, requester_ip, date))
-			return jsonify({"url": f"{request.url_root}shortener/{name}/"})
-		else:
-			return "Invalid url provided", 406
-	else:
-		return jsonify({"url":None, "shortener":"api"})
-'''
-
-#bruh get the shortened url from db and redirect to original url
 @app.route("/shortener/<string:name>/")
 def urlredi(name):
 	data = cur.execute("SELECT * FROM shortlmao WHERE shortened = %s",(f"{request.url_root}shortener/{name}/",))
