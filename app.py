b81feb3205b9750880fef2cf865b37a017db9a08
dammi <36568916+Dammibruh@users.noreply.github.com>
2020-07-27 01:48:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -40,6 +40,15 @@ conn = psycopg2.connect(uri)
 cur = conn.cursor()
 cur.execute("CREATE TABLE IF NOT EXISTS shortlmao (url TEXT, shortened TEXT, requester TEXT, date TEXT)")
 
+
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  return response
+
+
 @app.route("/home/")
 @app.route("/")
 def main():
